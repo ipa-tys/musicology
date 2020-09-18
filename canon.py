c85fb39e7c1498866b547f2a00a28a3e5b0679a0
Tobias Sing <tobiassing@TobiasMacbookPrivate.local>
2020-09-19 00:10:18 +0200

    creating antecedents for canon in the lower octave

--- canon.py
+++ canon.py
@@ -1,11 +1,22 @@
 from music21 import *
 from midi2audio import FluidSynth
-
+import random
 import pdb
+import sys
 
-legalIntervals = []
-intervalNames = 
-i = interval.Interval('M3')
+legalNoteNamesTenor = ['B-2','B2','C3','D3','E3','F3','G3','A3','B-3','B3','C4','D4','E4','F4','G4','A4']
+legalNoteNamesSoprano = ['B-3','B3','C4','D4','E4','F4','G4','A4','B-4','B4','C5','D5','E5','F5','G5','A5']
+legalNotes = []
+for legalNote in legalNoteNamesSoprano:
+    note1 = note.Note(legalNote)
+    note1.duration = note.duration.Duration(4.0)
+    legalNotes.append(note1)
+
+
+intervalNames = ['m-3','M-3','P-4','m3','M3','P5']
+intervals = []
+for intervalName in intervalNames:
+    intervals.append(interval.Interval(intervalName))
 
 fs = FluidSynth('Papelmedia_Irina_Brochin.sf2')
 
@@ -15,19 +26,29 @@ note1.duration = note.duration.Duration(4.0)
 stream1 = stream.Stream()
 stream1.append(note1)
 
-notes=[]
 for i in range(10):
-    note2 = note.Note('D3')
-    note2.duration = note.duration.Duration(4.0)
-    stream1.append(note2)
+    legal = False
 
+    while not legal:
+        i = random.choice(intervals)
+        i.noteStart = stream1[-1]
+        if i.noteEnd in legalNotes:
+            legal = True
+            note1 = i.noteEnd
+            note1.duration = note.duration.Duration(4.0)
+            stream1.append(note1)
 
-# fp = s.write('midi', fp='bla.mid')
+fp = stream1.write('midi', fp='canon1.mid')
+stream1.show()
+
+# sys.exit(0)
 
 # pdb.set_trace()
 
+my_tempo = tempo.MetronomeMark(number=40)
+stream1.insert(0, my_tempo)
 cadence_midi = stream1.write('midi')
-fs = FluidSynth('Papelmedia_Irina_Brochin.sf2') # arch
-fs.midi_to_audio(cadence_midi, 'canon.wav')
+fs = FluidSynth('~/soundfonts/papelmedia_Irina_Brochin.sf2') 
+fs.midi_to_audio(cadence_midi, 'canon1.wav')
 
 
